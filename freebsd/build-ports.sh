f14216c1bbfd2f29146d2933b47edd2ce8110410
Ronald Klop <ronald@klop.ws>
2021-07-12 13:45:35 +0200

    Try if it works to put the env var in make.conf.

--- freebsd/build-ports.sh
+++ freebsd/build-ports.sh
@@ -56,6 +56,7 @@ MAKE_JOBS_NUMBER=4
 MAKE_JOBS_NUMBER=4
 .endif
 #JAVA_VERSION=11
+IGNORE_OSVERSION=yes
 " > ${JAIL_PATH}/usr/local/etc/poudriere.d/make.conf
 echo "${PORTS}" > ${JAIL_PATH}/usr/local/etc/poudriere.d/port-list
 cp freebsd/poudriere.conf ${JAIL_PATH}/usr/local/etc/
