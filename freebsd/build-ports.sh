a275818621e67764be84e4fe74dc38adcb9fb668
Ronald Klop <ronald@klop.ws>
2021-07-12 13:31:09 +0200

    Try to set IGNORE_OSVERSION in the poudriere jail environment.

--- freebsd/build-ports.sh
+++ freebsd/build-ports.sh
@@ -80,4 +80,5 @@ jail -v -cm "name=${JAIL_NAME}_lighttpd" persist "path=${JAIL_PATH}" mount.devfs
     command=/usr/local/etc/rc.d/lighttpd onerestart
 
 jexec ${JAIL_NAME} pkg fetch -y -o "/usr/local/poudriere/data/packages/$POUDRIERE_NAME-custom" llvm10 llvm11 rust go gcc10
+sed -i .sed.bak "s/:setenv=/:setenv=IGNORE_OSVERSION=yes,/" "${JAIL_PATH}/usr/local/poudriere/jails/$POUDRIERE_NAME/etc/login.conf"
 jexec ${JAIL_NAME} poudriere bulk -j "$POUDRIERE_NAME" -p custom -f /usr/local/etc/poudriere.d/port-list
