35d22c9b9d4b2e85cef32d9c26c057c63c1fc863
Ronald Klop <ronald@klop.ws>
2023-08-16 21:39:44 +0200

    whitespace and reordering

--- freebsd/world/build.sh
+++ freebsd/world/build.sh
@@ -11,6 +11,7 @@ FETCH_ARGS=$( test ! -f base.txz || echo "-i base.txz" )
 JAIL_VERSION=14.0-CURRENT
 ARCH=$(uname -m)
 NUM_CPUS=$(sysctl -n kern.smp.cpus)
+
 if test "${JAIL_VERSION#*-}" = "RELEASE"; then
     SNAPSHOT_URL="https://download.freebsd.org/releases/${ARCH}/${JAIL_VERSION}/base.txz"
 else
@@ -22,11 +23,12 @@ if test ! COPYRIGHT -nt base.txz; then
     tar xmf base.txz
     find . \( -path ./dev -o -path ./usr/src -o -path ./usr/obj \) -prune -o ! -newer base.txz -ls
 fi
-mkdir -p dev
 
 trap 'jail -vr ${JAIL_NAME}' EXIT
 
+mkdir -p dev
 jail -vcmr name=${JAIL_NAME} persist path=${JAIL_PATH} mount.devfs devfs_ruleset=0 ip4=inherit
+
 echo "
 #COMPILER_TYPE=clang
 CC=/usr/local/bin/clang${LLVM_VER}
@@ -34,6 +36,7 @@ CXX=/usr/local/bin/clang++${LLVM_VER}
 CPP=/usr/local/bin/clang-cpp${LLVM_VER}
 LD=/usr/local/bin/ld.lld${LLVM_VER}
 " > ${JAIL_PATH}/etc/make.conf
+
 echo "
 NO_INSTALLEXTRAKERNELS=no
 KERNCONF=GENERIC-NODEBUG GENERIC
@@ -55,18 +58,24 @@ WITHOUT_CROSS_COMPILER=yes
 #WITHOUT_CLEAN=yes
 WITHOUT_TESTS=yes
 " > ${JAIL_PATH}/etc/src.conf
+
 # jexec ${JAIL_NAME} rm -f /usr/bin/cc /usr/bin/c++
 cp -p /etc/resolv.conf ${JAIL_PATH}/etc/
+
 pkg -j ${JAIL_NAME} upgrade -y
 pkg -j ${JAIL_NAME} install -y ${CROSS_TOOLCHAIN} # byacc
+#cd ${JAIL_PATH}/usr/bin && ln -s ../local/bin/yacc
+
 jexec ${JAIL_NAME} sh -c "yes | /usr/bin/make -C /usr/src delete-old delete-old-libs"
 cd ${JAIL_PATH}/usr/bin && rm -f cc CC c++ cpp
-#cd ${JAIL_PATH}/usr/bin && ln -s ../local/bin/yacc
+
 jexec ${JAIL_NAME} /usr/bin/make -C /usr/src -j${NUM_CPUS} buildworld buildkernel
-cp -p /etc/resolv.conf ${JAIL_PATH}/etc/
+
 sed -i .sed.bak s/quarterly/latest/ ${JAIL_PATH}/etc/pkg/FreeBSD.conf
+
 # clean up old builds
 test -d ${JAIL_PATH}/usr/obj/usr/src/repo && rm -r ${JAIL_PATH}/usr/obj/usr/src/repo
 jexec ${JAIL_NAME} /usr/bin/make -C /usr/src -j${NUM_CPUS} packages
+
 trap - EXIT
 jail -vr ${JAIL_NAME}
