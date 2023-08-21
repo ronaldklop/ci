e93288678535311829d620a06d9da022aa2c335d
Ronald Klop <ronald@klop.ws>
2023-08-21 20:30:21 +0200

    make things work. needs cleanup.

--- freebsd/world/build.sh
+++ freebsd/world/build.sh
@@ -10,7 +10,7 @@ CROSS_TOOLCHAIN=llvm${LLVM_VER}
 FETCH_ARGS=$( test ! -f base.txz || echo "-i base.txz" )
 JAIL_VERSION=14.0-CURRENT
 ARCH=$(uname -m)
-NUM_CPUS=$(sysctl -n kern.smp.cpus)
+NUM_CPUS=${NUM_CPUS:-$(sysctl -n kern.smp.cpus)}
 
 if test "${JAIL_VERSION#*-}" = "RELEASE"; then
     SNAPSHOT_URL="https://download.freebsd.org/releases/${ARCH}/${JAIL_VERSION}/base.txz"
@@ -29,45 +29,52 @@ trap 'jail -vr ${JAIL_NAME}' EXIT
 mkdir -p dev
 jail -vcmr name=${JAIL_NAME} persist path=${JAIL_PATH} mount.devfs devfs_ruleset=0 ip4=inherit
 
-echo "
-#COMPILER_TYPE=clang
-CC=/usr/local/bin/clang${LLVM_VER}
-CXX=/usr/local/bin/clang++${LLVM_VER}
-CPP=/usr/local/bin/clang-cpp${LLVM_VER}
-LD=/usr/local/bin/ld.lld${LLVM_VER}
-" > ${JAIL_PATH}/etc/make.conf
+LLVM_DIR=/usr/local/${CROSS_TOOLCHAIN}
+
+echo "" > ${JAIL_PATH}/etc/make.conf
+echo "" > ${JAIL_PATH}/etc/src.conf
 
 echo "
-NO_INSTALLEXTRAKERNELS=no
-KERNCONF=GENERIC-NODEBUG GENERIC
-#CROSS_TOOLCHAIN=${CROSS_TOOLCHAIN}
+CROSS_TOOLCHAIN=${CROSS_TOOLCHAIN}
 
-#WITHOUT_TOOLCHAIN=yes
+WITHOUT_TOOLCHAIN=yes
+#LD=${LLVM_DIR}/bin/ld.lld
+#CC=${LLVM_DIR}/bin/clang
+#CXX=${LLVM_DIR}/bin/clang++
+#CPP=${LLVM_DIR}/bin/clang-cpp
+#OBJCOPY=/usr/local/bin/objcopy
 
-WITHOUT_CLANG=yes
-WITHOUT_CLANG_EXTRAS=yes
-WITHOUT_CLANG_FORMAT=yes
-WITHOUT_CLANG_FULL=yes
-WITHOUT_LLD=yes
-WITHOUT_LLDB=yes
-WITHOUT_LLVM_BINUTILS=yes
-WITHOUT_LLVM_COV=yes
+#CFLAGS+=-target x86_64-unknown-freebsd14.0
+#CFLAGS+=--sysroot=/data/jails/builder/usr/obj/usr/src/amd64.amd64/tmp
+#CFLAGS+=-B/data/jails/builder/usr/obj/usr/src/amd64.amd64/tmp/usr/bin
+#WITHOUT_CROSS_COMPILER=yes
+#WITHOUT_SYSTEM_COMPILER=yes
+#WITHOUT_SYSTEM_LINKER=yes
 
-WITHOUT_CROSS_COMPILER=yes
-
+#WITHOUT_TESTS=yes
 #WITHOUT_CLEAN=yes
-WITHOUT_TESTS=yes
-" > ${JAIL_PATH}/etc/src.conf
+" > ${JAIL_PATH}/etc/make.conf
 
 # jexec ${JAIL_NAME} rm -f /usr/bin/cc /usr/bin/c++
 cp -p /etc/resolv.conf ${JAIL_PATH}/etc/
 
-pkg -j ${JAIL_NAME} upgrade -y
-pkg -j ${JAIL_NAME} install -y ${CROSS_TOOLCHAIN} # byacc
-#cd ${JAIL_PATH}/usr/bin && ln -s ../local/bin/yacc
+pkg -j ${JAIL_NAME} delete -a -y
 
-jexec ${JAIL_NAME} sh -c "yes | /usr/bin/make -C /usr/src delete-old delete-old-libs"
-cd ${JAIL_PATH}/usr/bin && rm -f cc CC c++ cpp
+#cd ${JAIL_PATH}/usr/bin && rm -f cc CC c++ cpp
+
+jexec ${JAIL_NAME} sh -c "yes | /usr/bin/make CC=${LLVM_DIR}/bin/clang LD=${LLVM_DIR}/bin/ld.lld -C /usr/src delete-old delete-old-libs"
+
+cd ${JAIL_PATH}/usr/bin && ln -fs ../local/llvm16/bin/clang cc
+cd ${JAIL_PATH}/usr/bin && ln -fs ../local/llvm16/bin/clang CC
+cd ${JAIL_PATH}/usr/bin && ln -fs ../local/llvm16/bin/clang++ c++
+cd ${JAIL_PATH}/usr/bin && ln -fs ../local/llvm16/bin/clang-cpp cpp
+cd ${JAIL_PATH}/usr/bin && ln -fs ../local/llvm16/bin/llvm-objcopy objcopy
+cd ${JAIL_PATH}/usr/bin && ln -fs ../local/llvm16/bin/ld
+
+#pkg -j ${JAIL_NAME} delete -y binutils
+pkg -j ${JAIL_NAME} install -y ${CROSS_TOOLCHAIN} byacc
+# binutils
+cd ${JAIL_PATH}/usr/bin && ln -s ../local/bin/yacc
 
 jexec ${JAIL_NAME} /usr/bin/make -C /usr/src -j${NUM_CPUS} buildworld buildkernel
 
@@ -75,7 +82,7 @@ sed -i .sed.bak s/quarterly/latest/ ${JAIL_PATH}/etc/pkg/FreeBSD.conf
 
 # clean up old builds
 test -d ${JAIL_PATH}/usr/obj/usr/src/repo && rm -r ${JAIL_PATH}/usr/obj/usr/src/repo
-jexec ${JAIL_NAME} /usr/bin/make -C /usr/src -j${NUM_CPUS} packages
+#jexec ${JAIL_NAME} /usr/bin/make -C /usr/src -j${NUM_CPUS} packages
 
 trap - EXIT
 jail -vr ${JAIL_NAME}
