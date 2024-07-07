75165fc5a893156b3394f3b3fc3e7972cd821cee
Ronald Klop <ronald@klop.ws>
2024-07-07 23:11:18 +0200

    switch back to poudriere bulk and see if 'make test' is also done here

--- freebsd/build-ports.sh
+++ freebsd/build-ports.sh
@@ -96,7 +96,7 @@ if ! jexec ${JAIL_NAME} poudriere jail -i -j "$POUDRIERE_NAME"; then
 fi
 jexec ${JAIL_NAME} poudriere jail -u -j "$POUDRIERE_NAME"
 
-#jexec ${JAIL_NAME} nice -n 15 poudriere bulk -j "$POUDRIERE_NAME" -p custom -f /usr/local/etc/poudriere.d/port-list -b latest -t
-for p in ${PORTS}; do
-    jexec ${JAIL_NAME} nice -n 20 poudriere testport -j "$POUDRIERE_NAME" -p custom -b latest -o $p
-done
+jexec ${JAIL_NAME} nice -n 15 poudriere bulk -j "$POUDRIERE_NAME" -p custom -f /usr/local/etc/poudriere.d/port-list -b latest -t
+#for p in ${PORTS}; do
+#    jexec ${JAIL_NAME} nice -n 20 poudriere testport -j "$POUDRIERE_NAME" -p custom -b latest -o $p
+#done
