bf3111261addbb5b9699b6afbea359ea19843ee6
Ronald Klop <ronald@klop.ws>
2025-09-23 13:41:36 +0200

    debug output

--- freebsd/build-ports.sh
+++ freebsd/build-ports.sh
@@ -109,7 +109,7 @@ then
     touch ${LASTUPDATE}
 fi
 
-jexec ${JAIL_NAME} nice -n 15 poudriere bulk -j "$POUDRIERE_NAME" -p custom -f /usr/local/etc/poudriere.d/port-list -b latest -t
+jexec ${JAIL_NAME} nice -n 15 poudriere bulk -vv -j "$POUDRIERE_NAME" -p custom -f /usr/local/etc/poudriere.d/port-list -b latest -t
 #for p in ${PORTS}; do
 #    jexec ${JAIL_NAME} nice -n 20 poudriere testport -j "$POUDRIERE_NAME" -p custom -b latest -o $p
 #done
